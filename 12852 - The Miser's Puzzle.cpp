fc71f591f33b64af920b384ad1b774b9316dc9b3
Ashikur Rahman <iamashik423@gmail.com>
2024-11-10 12:45:42 +0600

    Create 12852 - The Miser's Puzzle.cpp

---
+++ 12852 - The Miser's Puzzle.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+#define imax INT_MAX
+#define llmax INT64_MAX
+#define ll long long
+#define stpr(x) setprecision(x)
+#define sorted(x) sort(x.begin() , x.end())
+#define rsorted(x) sort(x.rbegin() , x.rend())
+#define nextp(x) next_permutation(x.begin(), x.end())
+#define _rand(x,y) min(x,y)+rand()%(abs(y-x)+1)
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pss pair<string, string>
+#define psi pair<string, int>
+#define pis pair<int, string>
+#define mod 1000000007
+#define pie 3.141592653589793238
+#define e_ 2.718281828459045235
+
+using namespace std;
+
+void solve(){
+    ll t; cin >> t;
+    for(ll i = 1; i <= t; i++){
+        cout << "Case " << i << ": ";
+        ll n; cin >> n;
+        ll ans;
+        for(int i = 0; i < n; i++){
+            ll t; cin >> t;
+            t *= 35;
+            if(i == 0)
+                ans = t;
+            else
+                ans = ans*t/__gcd(ans,t);
+        }
+        cout << ans << endl;
+    }
+}
+
+int main(){
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+}
